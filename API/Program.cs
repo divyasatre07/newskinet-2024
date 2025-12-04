a743ca3729f54f23783c797b5c0dd1b0d5b64064
Nilesh Satre <nilesh123@gmail.com>
2025-12-03 23:53:58 -0800

    end of section 2

---
+++ API/Program.cs
@@ -0,0 +1,30 @@
+
+
+using Infrastructure.Data; // Make sure you include your StoreContext
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+// Add DbContext
+builder.Services.AddDbContext<StoreContext>(opt =>
+{
+    opt.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.MapControllers();
+
+app.Run();
